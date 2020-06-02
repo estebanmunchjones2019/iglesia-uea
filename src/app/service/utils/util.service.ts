f5a109f950489c352754235e65f805147a21bb98
sgallardo <sgallardo@todo1.net>
2020-06-02 19:13:45 -0300

    Fix video url

--- src/app/service/utils/util.service.ts
+++ src/app/service/utils/util.service.ts
@@ -7,14 +7,16 @@ export class UtilService {
   constructor(private _sanitizer: DomSanitizer) {}
 
   getVideoIframe(url: string) {
-    var video, results;
+    var video, results, time;
 
     if (url === null) {
       return '';
     }
-    results = url.match('[\\?&]v=([^&#]*)');
+    url = url.replace('&feature=youtu.be', '');
+    results = url.match('[\\?&]v=([^#]*)');
     video   = (results === null) ? results = url.substring(url.lastIndexOf('/') + 1, url.length) : results[1];
-    video = (video === null) ? video : video.replace('?t=', '?start=')
+    video = video.replace('?t=', '?start=');
+    video = video.replace('&t=', '?start=')
     return this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + video);
   }
 }
